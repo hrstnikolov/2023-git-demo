ca42a045eec1a9bbfd5701733b585c576f0ab03f
Ivan Ivanov <ivan@gmail.com>
2023-08-21 18:42:17 +0300

    deleted a line in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def main():
     print('I am the main app!')
     print('I am running all of it!')
-    print('Ivan updated this')
+
 
 if __name__=='__main__':
     print(f'Current time is:{datetime.now()}')
