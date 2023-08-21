02c2ef2e58a19bb03261c6e37d103840e5943fca
hristo-nikolov-personal <hristo@gmail.com>
2023-08-21 18:08:25 +0300

    changes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from datetime import datetime  
+
+
 def main():
     print('I am the main app')
 
 
 if __name__=='__main__':
+    print(f'Current time is:{datetime.now()}')
     main()
---
+++ operations/start.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+if __name__=='__main__':
+    print('Starting operations..')
+    sleep(2)
+    print('Starting completed!')
