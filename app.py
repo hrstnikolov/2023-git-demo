645ccc8509bf46ea3d7839e3040d78bd3f215260
Ivan Ivanov <ivan@gmail.com>
2023-08-21 20:53:30 +0300

    added a new command

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 def main():
     print('I am the main app!')
     print('I am running all of it!')
+    pring('adding new command here as well')
 
 
 if __name__=='__main__':
--- operations/start.py
+++ operations/start.py
@@ -4,3 +4,4 @@ if __name__=='__main__':
     print('Starting operations..')
     sleep(2)
     print('Starting completed! All done!')
+    print('added new command after start is loaded')
